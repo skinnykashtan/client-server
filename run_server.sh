55eceb50e55af5a46735d3075bf02848858d7602
skinnykashtan <veestereh@gmail.com>
2026-06-22 20:07:55 +0200

    add build script

---
+++ run_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+cmake -S . -B build
+cmake --build build
+./build/server
--- src/server.cpp
+++ src/server.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <string.h>
 
 int main() {
     const int PORT = 3000;
@@ -21,7 +22,7 @@ int main() {
     serverAddr.sin_addr.s_addr = INADDR_ANY;
     serverAddr.sin_port = htons(PORT);
 
-    if (bind(serverFd, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) == -1) {
+    if (bind(serverFd, reinterpret_cast<sockaddr *>(&serverAddr), sizeof(serverAddr)) == -1) {
         std::cerr << "bind() failed: " << std::strerror(errno) << '\n';
         close(serverFd);
         return 1;
@@ -48,6 +49,34 @@ int main() {
     }
 
     std::cout << "Client connected, clientFd = " << clientFd << '\n';
+
+    char buffer[1024];
+
+    while (true) {
+        ssize_t bytesReceived = recv(clientFd, buffer, sizeof(buffer) - 1, 0);
+
+        if (bytesReceived == -1) {
+            std::cerr << "recv() failed: " << std::strerror(errno) << '\n';
+            break;
+        }
+
+        if (bytesReceived == 0) {
+            std::cout << "Client disconnected" << '\n';
+            break;
+        }
+
+        buffer[bytesReceived] = '\0';
+
+        buffer[strcspn(buffer, "\r\n")] = '\0';
+
+        if (std::strcmp(buffer, "/ping") == 0) {
+            const char* response = "pong";
+            send(clientFd, response, std::strlen(response), 0);
+        }
+
+        std::cout << "Received from client: " << buffer << '\n';
+    }
+
 
     close(clientFd);
     close(serverFd);
